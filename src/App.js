acaf64cce165c11c00120fa7f7db9ea3a0fa8718
Antonio Alaniz <antonio.alaniz@gmail.com>
2019-12-31 11:46:18 -0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react today! No, seriously!
+          I was changed on the feature branch!
         </a>
       </header>
     </div>
